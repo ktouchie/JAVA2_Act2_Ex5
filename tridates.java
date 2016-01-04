70fc589659308c080805cd239c092425f2052e5b
Karin Touchie <ktouchie@gmail.com>
2016-01-04 10:44:17 +0100

    Add bubble loop

--- tridates.java
+++ tridates.java
@@ -7,10 +7,26 @@ class tridates {
         String pattern = "dd/MM/yyyy";
         SimpleDateFormat format = new SimpleDateFormat(pattern);
         
-        for (i = 0; i<array.length-1; i++) {
-            Date date = format.parse(array[i]);
-            Date date2 = format.parse(array[i+1]);
-        }
+        boolean swap = false;
+        
+        do {
+            swap = false;
+            
+            for (i = 0; i<array.length-1; i++) {
+                Date date = format.parse(array[i]);
+                Date date2 = format.parse(array[i+1]);
+                
+                if (date>date2) {
+                    Date x = date;
+                    date = date2;
+                    date2 = x;
+                    swap = true;
+                }
+            }            
+            
+        } while swap == true;
+        
+        System.out.println(Arrays.toString(array))
         
         
     }
