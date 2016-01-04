ae7e334ce3647a040dbbba34002bd1063e88d822
Karin Touchie <ktouchie@gmail.com>
2016-01-04 11:06:41 +0100

    Remove SimpleDateFormat, convert substrings to integers

--- tridates.java
+++ tridates.java
@@ -4,29 +4,42 @@ class tridates {
     public static void main (String[] args) {
         
         String[] array = {"12/04/1982", "09/01/1944", "06/01/1985", "12/04/1980", "29/08/1942", "04/03/1980"};
-        String pattern = "dd/MM/yyyy";
-        SimpleDateFormat format = new SimpleDateFormat(pattern);
         
         boolean swap = false;
         
         do {
             swap = false;
             
-            for (i = 0; i<array.length-1; i++) {
-                Date date = format.parse(array[i]);
-                Date date2 = format.parse(array[i+1]);
+            for (int i = 0; i<array.length-1; i++) {
                 
-                if (date>date2) {
-                    Date x = date;
-                    date = date2;
-                    date2 = x;
+                int year1 = Integer.parseInt(array[i].substring(6,10));
+                int year2 = Integer.parseInt(array[i+1].substring(6,10));
+
+                
+                if (year1==year2) {
+                    
+                    int month1 = Integer.parseInt(array[i].substring(3,5));
+                    int month2 = Integer.parseInt(array[i+1].substring(3,5));
+                    
+                    if (month1>month2) {
+                        String x = array[i];
+                        array[i] = array[i+1];
+                        array[i+1] = x;
+                        swap = true;
+                    }
+                
+                }
+                else if (year1>year2){
+                    String x = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = x;
                     swap = true;
                 }
             }            
             
-        } while swap == true;
+        } while (swap == true);
         
-        System.out.println(Arrays.toString(array))
+        System.out.println(Arrays.toString(array));
         
         
     }
